41eb122c1984a40482a898b2f8501ca8cb01452b
GabrielleCGNeves <gabriellecarvallhoneves@gmail.com>
2023-03-27 23:54:08 -0300

    :sparkles: created concrete class

---
+++ src/php/Assalariado.php
@@ -0,0 +1,13 @@
+<?php  
+
+    require 'Empregado.php';
+
+    class Assalariado extends Empregado{
+        var $salario;
+
+        function __construct($nome, $sobrenome, $cpf, $salario){
+            parent:: __construct($nome, $sobrenome, $cpf);
+            $this -> salario = $salario;
+        }
+    }
+?>
