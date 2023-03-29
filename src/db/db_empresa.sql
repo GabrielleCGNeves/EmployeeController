1615e77374ef8335587be54972bbc200e9a343f6
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-03-29 00:17:57 -0300

    :card_file_box: added sql querys

---
+++ src/db/db_empresa.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE DB_Empresa;
+USE DB_Empresa;
+
+CREATE TABLE TB_Colaborador(
+    colab_cod INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    colab_nome VARCHAR(20) NOT NULL,
+    colab_sobrenome VARCHAR(40) NOT NULL,
+    colab_cpf VARCHAR(11) NOT NULL,
+    colab_salLiq FLOAT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE utf8_unicode_ci;
