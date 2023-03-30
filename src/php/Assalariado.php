fba291ee6f277e2595bb5633e8c423060fa2befb
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-03-29 23:07:00 -0300

    :recycle: updated function

--- src/php/Assalariado.php
+++ src/php/Assalariado.php
@@ -1,6 +1,6 @@
 <?php  
 
-    require 'Empregado.php';
+    require_once 'Empregado.php';
 
     class Assalariado extends Empregado{
         var $salario;
@@ -11,7 +11,21 @@
         }
 
         function vencimento(){
-            return($this -> salario);
+            if ($this -> salario > 3856.95){
+                return($this -> salario - ($this -> salario * 0.14));
+            }
+            
+            else if($this -> salario < 3856.95 && $this -> salario > 2571.30){
+                return($this -> salario - ($this -> salario  * 0.12));
+            }
+
+            else if($this -> salario < 2571.30 && $this -> salario >1302.01){
+                return($this -> salario - ($this -> salario * 0.09));
+            }
+
+            else{
+                return($this -> salario - ($this -> salario  * 0.075));
+            }
         }
     }
 ?>
