85eec00094ab5d55f62d9bd3c3f25d42828f0770
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-04-05 22:04:19 -0300

    :sparkles: added ´mostrarFormulario() and mostrarConsulta()´

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,31 @@ function mostrarCampos(){
         campoHorista.style.display = "none";
     }
 }
+
+function mostrarFormulario() {
+    var forms = document.getElementById("forms");
+    
+    if (forms.style.display === "none") {
+      forms.style.display = "block";
+      consulta.style.display = "none";
+    } 
+    
+    else {
+      forms.style.display = "none";
+      consulta.style.display = "none";
+    }
+}
+
+function mostrarConsulta() {
+    var consulta = document.getElementById("consulta");
+    
+    if (consulta.style.display === "none") {
+      consulta.style.display = "block";
+      forms.style.display = "none";
+    } 
+    
+    else {
+      consulta.style.display = "none";
+      forms.style.display = "none";
+    }
+}
